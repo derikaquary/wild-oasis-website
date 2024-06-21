0d232a8f5826813a01ba3d95b28f770729557dbc
derikaquary <derikaquary@gmail.com>
2024-06-22 07:14:42 +0800

    Initial commit

---
+++ app/_components/UpdateProfileForm.js
@@ -0,0 +1,71 @@
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
+
+import { updateGuest } from "../_lib/actions";
+import SubmitButton from "./SubmitButton";
+
+function UpdateProfileForm({ guest, children }) {
+  const [count, setCount] = useState();
+
+  const { countryFlag, fullName, email, nationality, nationalID } = guest;
+
+  return (
+    <form
+      action={updateGuest}
+      className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col">
+      <div className="space-y-2">
+        <label>Full name</label>
+        <input
+          disabled
+          defaultValue={fullName}
+          name="fullName"
+          className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
+        />
+      </div>
+
+      <div className="space-y-2">
+        <label>Email address</label>
+        <input
+          disabled
+          defaultValue={email}
+          name="email"
+          className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
+        />
+      </div>
+
+      <div className="space-y-2">
+        <div className="relative flex items-center justify-between">
+          <label htmlFor="nationality">Where are you from?</label>
+          <div className="relative h-5 aspect-video">
+            <Image
+              src={countryFlag}
+              alt="Country flag"
+              fill
+              className="object-cover"
+            />
+          </div>
+        </div>
+      </div>
+
+      {children}
+      {/* this children is <SelectCountry/> */}
+
+      <div className="space-y-2">
+        <label htmlFor="nationalID">National ID number</label>
+        <input
+          defaultValue={nationalID}
+          name="nationalID"
+          className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
+        />
+      </div>
+
+      <div className="flex justify-end items-center gap-6">
+        <SubmitButton pendingLabel="...Updating">Update Profile</SubmitButton>
+      </div>
+    </form>
+  );
+}
+
+export default UpdateProfileForm;
