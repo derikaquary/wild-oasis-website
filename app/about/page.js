12a03459722253a3bb0d3bdb5def07cb2497b35c
derikaquary <derikaquary@gmail.com>
2024-06-23 15:59:40 +0800

    making the about page responsive

--- app/about/page.js
+++ app/about/page.js
@@ -14,7 +14,7 @@ export default async function Page() {
   const cabins = await getCabins();
 
   return (
-    <div className="grid grid-cols-5 gap-x-24 gap-y-32 text-lg items-center">
+    <div className="grid grid-cols-1 sm:grid-cols-5 sm:gap-x-24 gap-y-32 text-lg justify-center sm:items-center">
       <div className="col-span-3">
         <h1 className="text-4xl mb-10 text-accent-400 font-medium">
           Welcome to The Wild Oasis
